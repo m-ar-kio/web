57e1fea14ddd9eb0254d7953772983bc787ec07a
Chris Seifert <25006584+seichris@users.noreply.github.com>
2021-10-26 22:02:27 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,18 +7,14 @@ function About() {
     <Layout title="About">
       <div style={{ padding: "0 200px" }}>
         <h1>About m-ar-k</h1>
-        <p style={pstyle}>Mark is a bookmark tool running on arweave.</p>
+        <p style={pstyle}>Mark is a bookmark manager that allows you to save and share any URL on Arweave. In our next version, Mark will automatically import your liked Tweets and suggest personalized content, based on your past likes.</p>
         <p style={pstyle}>
-          Every time you create a mark content, you spend fee and 0.01 AR for
-          PST fee which will be distributed to $MARK holders, but you'll get one
-          claimable $MARK(not claimable for now).
+          Saving links from different platforms will increase our algorithms prediction quality. This way, eventually our content suggestions will be more accurate than those of any single centralized data silo.
         </p>
         <p style={pstyle}>
-          You can see marks flow and your own marks on the official website. If
-          you found any mark helps you, you can also "buy a cup of coffee" for
-          the marker.
+          Running on Arweave, Mark aims to reward all stakeholders - users curating content, algorithm engineers, and developers integrating the content suggestions into the Twitter UI. Every time you create a Mark, you will receive 1 $MARK and spend 0.01 AR as PST fee which will be distributed to $MARK holders. ($MARK is currently not claimable.) You can also tip Markers for great content suggestions.
         </p>
-        <p style={pstyle}>Happy to mark!</p>
+        <p style={pstyle}>Happy marking!</p>
       </div>
     </Layout>
   )
